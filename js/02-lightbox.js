567e0d4b1825883f8a2aa2c18a53306a7964792b
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-03-25 16:13:10 +0200

    add lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesEl = document.querySelector('ul.gallery');
+
+function createGalleryMarkup(items) {
+    return items
+        .map(
+            ({ preview, original, description }) => `
+            <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            </li>
+        `,
+        )
+        .join('');
+}
+
+const addGalleryMarkyp = createGalleryMarkup(galleryItems);
+
+imagesEl.innerHTML = addGalleryMarkyp;
+
+imagesEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+    blockStandartAction(evt);
+
+    const isGalleryItemEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryItemEl) {
+        return;
+    }
+
+    console.log(evt.target);
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+    gallery.on('show.simplelightbox', function () {});
+    gallery.open();
+    // gallery.on('close.simplelightbox', function () {});
+    // gallery.close();
+}
+
+function blockStandartAction(evt) {
+    evt.preventDefault();
+}
